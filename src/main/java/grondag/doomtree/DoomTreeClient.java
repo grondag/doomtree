b4a921bb3d66f8b09aed21a8bc517f69250822f6
Grondag <grondag@gmail.com>
2019-11-03 09:51:02 -0800

    Fix icor flowing render

--- src/main/java/grondag/doomtree/DoomTreeClient.java
+++ src/main/java/grondag/doomtree/DoomTreeClient.java
@@ -1,16 +1,16 @@
 /*******************************************************************************
  * Copyright (C) 2019 grondag
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -91,7 +91,7 @@ public class DoomTreeClient implements ClientModInitializer {
 		ClientSidePacketRegistry.INSTANCE.register(DoomS2C.IDENTIFIER, DoomS2C::handle);
 		ClientSidePacketRegistry.INSTANCE.register(XpDrainS2C.IDENTIFIER, XpDrainS2C::handle);
 
-		REGISTRAR.fluidRenderHandler(DoomFluids.ICHOR, DoomFluids.ICHOR_COLOR, "block/water_still", "block/water_flow");
+		REGISTRAR.fluidRenderHandler(DoomFluids.ICHOR, DoomFluids.FLOWING_ICHOR, DoomFluids.ICHOR_COLOR, "block/water_still", "block/water_flow");
 
 		ParticleFactoryRegistry.getInstance().register(DoomParticles.ALCHEMY_IDLE, IdleParticleFactory::new);
 		ParticleFactoryRegistry.getInstance().register(DoomParticles.ALCHEMY_WAKING, WakingParticleFactory::new);
